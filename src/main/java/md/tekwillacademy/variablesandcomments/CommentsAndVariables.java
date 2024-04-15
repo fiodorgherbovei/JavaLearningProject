4eeda19cb30c8031f4a4902d52e19e9c59e08502
fiodorgherbovei <tudor.gherbovei@mail.ru>
2024-04-15 18:21:24 +0300

    added 2 new bolean variables and a practical exercise

---
+++ src/main/java/md/tekwillacademy/variablesandcomments/CommentsAndVariables.java
@@ -0,0 +1,14 @@
+package md.tekwillacademy.variablesandcomments;
+
+public class CommentsAndVariables {
+    public static void main(String[] args) {
+        boolean todayIsMonday;
+        todayIsMonday = true;
+        System.out.println("The value of our variables is :" + todayIsMonday);
+        boolean tomorrowIsTuesday= true;
+        System.out.println("The value of our variables is :" + tomorrowIsTuesday);
+    }
+
+
+
+}
