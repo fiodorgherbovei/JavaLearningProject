e2d23306205209b22b78a9a1db3bc8129f73da29
fiodorgherbovei <tudor.gherbovei@mail.ru>
2024-04-19 00:37:03 +0300

    Added the Java Code for an Auto Service app

---
+++ src/main/java/md/tekwillacademy/classesandobjects/autoservicetask/Tool.java
@@ -0,0 +1,14 @@
+package md.tekwillacademy.classesandobjects.autoservicetask;
+
+public class Tool {
+
+    String nameOfTheTool;
+
+    String material;
+
+    public Tool(String enteredNameOfTheTool){
+        nameOfTheTool = enteredNameOfTheTool;
+        System.out.println(" The constructor with param was used and the materia is still null");
+
+    }
+}
---
+++ src/main/java/md/tekwillacademy/classesandobjects/autoservicetask/Worker.java
@@ -0,0 +1,19 @@
+package md.tekwillacademy.classesandobjects.autoservicetask;
+
+public class Worker {
+
+    String name;
+
+    int yearOfExperience;
+
+    int pricePerHour;
+
+    public Worker (String name ){
+        this.name = name;
+
+    }
+
+    public Worker(){
+        System.out.println("Welcome, good job, you are the best");
+    }
+}
